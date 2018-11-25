09ae0b291d05fb0109393abfc48f46a14750aba5
unknown <damien.orlando@ynov.com>
2018-11-25 22:42:23 +0100

    Tentatives d'arguments d'opérations mathématiques, sans succès...

--- ORLANDO_Damien_Kholle_1.py
+++ ORLANDO_Damien_Kholle_1.py
@@ -5,20 +5,19 @@ import os
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", help="Show the values of the list", action="store_true")
-parser.add_argument("-a", help="Add your inputs to the list")
-parser.add_argument("-c", help="Erase the list", action="store_true")
-#parser.add_argument("-s --max", help="Show the biggest value of the list")
-#parser.add_argument("-s --min", help="Show the smallest value of the list")
-#parser.add_argument("-s --sum", help="Calculate the sum of the values")
-#parser.add_argument("-s --moy", help="Calculate the average of the values")
-#parser.add_argument("-t", help="Sort out the values in the ascending order")
-#parser.add_argument("-t --desc", help="Sort out the values in the descending order")
+gList = parser.add_argument_group('Manipulate the list')
+gList.add_argument("-l", help="Show the values of the list", action="store_true")
+gList.add_argument("-a", help="Add your inputs to the list", action="append")
+gList.add_argument("-c", help="Erase the list", action="store_true")
+gMath = parser.add_argument_group('Mathematical operations')
+# gMath.add_argument("-s", "--max", help="Show the biggest value of the list", action="store_true")
+# gMath.add_argument("-s", "--min", help="Show the smallest value of the list", action="store_true")
+# gMath.add_argument("-s", "--sum", help="Calculate the sum of the values", action="store_true")
+# gMath.add_argument("-s", "--moy", help="Calculate the average of the values", action="store_true")
 args = parser.parse_args()
 
 
 if args.a:
-    #for i in range(values.length):
     value = args.a
     with open("list.csv", mode="w", newline="") as val_list:
         list_writer = csv.writer(val_list)
@@ -36,3 +35,49 @@ if args.c:
         os.remove("list.csv")
     else:
         print("Nothing to remove!")
+
+# if args.s:
+#     if args.max:
+#         with open("list.csv", mode="r") as val_list:
+#             list_reader = csv.reader(val_list)
+#             maximum = 0
+#             holder = 0
+#
+#             for line in val_list:
+#                 holder = line
+#                 if int(holder) > maximum:
+#                     maximum = holder
+#         print (maximum)
+#
+#     if args.min:
+#         with open("list.csv", mode="r") as val_list:
+#             list_reader = csv.reader(val_list)
+#             minimum = 0
+#             holder = 0
+#
+#             for line in val_list:
+#                 holder = line
+#                 if int(holder) > minimum:
+#                     minimum = holder
+#         print (minimum)
+#
+#     if args.sum:
+#         with open("list.csv", mode="r") as val_list:
+#             list_reader = csv.reader(val_list)
+#             sum = 0
+#
+#             for line in val_list:
+#                 sum += int(line)
+#         print (sum)
+#
+#
+#     if args.moy:
+#         with open("list.csv", mode="r") as val_list:
+#             list_reader = csv.reader(val_list)
+#             sum = 0
+#             nbValues = 0
+#
+#             for line in val_list:
+#                 sum += int(line)
+#                 nbValues += 1
+#         print (sum/nbValues)
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Kholle-1.5-B2
+
+usage: ORLANDO_Damien_Kholle_1.py [-h] [-l] [-a A] [-c]
+
+  -h, --help  show this help message and exit
+
+Manipulate the list:
+  -l          Show the values of the list
+  -a A        Add your inputs to the list
+  -c          Erase the list
